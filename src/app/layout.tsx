f8114dacf3e013820fa6a107e1c9a03c6b332a0c
Luis Santana <lunasa2003@hotmail.com>
2023-06-28 03:52:49 +0200

    titulo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Nayobot - IA progresiva',
+  title: 'Nayobot - IA progresivaaa',
   description: 'Nayobot - IA progresiva',
 }
 
